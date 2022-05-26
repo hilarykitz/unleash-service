286effe077344690425cbfe3c5c64e6228a90424
Hilary Kitz <hilarykitz@gmail.com>
2022-05-26 10:39:10 +0100

    create tiny proxy server to test unleash

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const port = 3027;
 
 const { createApp } = require("@unleash/proxy");
 
 const app = createApp({
   unleashUrl: "http://localhost:4242/api",
-  unleashApiToken:
-    "*:*.fa56c94545b015752d414bb69954ebe6a49981a7883fdf06e3ea56e9",
+  unleashApiToken: process.env.API_TOKEN,
   clientKeys: ["proxy-client-key"],
 });
 
